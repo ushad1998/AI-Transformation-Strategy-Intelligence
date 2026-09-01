6df1c2b5d39b20889e91b3f552c069035ac96302
Usha D <ushapobrar@gmail.com>
2026-09-01 15:23:54 +0530

    Fix Vercel delpoyment configuration

--- app.py
+++ app.py
@@ -3,26 +3,24 @@ from pydantic import BaseModel
 
 from agent import generate_document
 
-# Create FastAPI app
 app = FastAPI(
-    title="Autonomous AI Agent API",
-    description="AI Agent that creates execution plans and generate word documents.",
-    version="1.0"
+    title="AI Transformation Strategy Intelligence API",
+    description="Autonomous AI agent for generating AI transformation strategy documents.",
+    version="1.0.0"
 )
 
-# Request Model
+
 class AgentRequest(BaseModel):
     request: str
 
 
-# Health Check
 @app.get("/")
 def home():
     return {
-        "message": "AI Agent API is running successfully!"
+        "message": "AI Transformation Strategy Intelligence API is running successfully!"
     }
 
-# Main API
+
 @app.post("/agent")
 def run_agent(data: AgentRequest):
     try:
